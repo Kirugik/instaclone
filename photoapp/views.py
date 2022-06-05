a23cd58d470393df26a2ec2418a33dce2b34f658
Robert Kirui <kirugik79@gmail.com>
2022-06-05 12:04:16 +0300

    Feat: Create sign-up, login, and logout view functions

--- photoapp/views.py
+++ photoapp/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User 
 from django.http import HttpResponse 
+from django.contrib import messages 
 from .models import Profile,Image,Comment,Like
 from .forms import UpdateProfileForm,NewPostForm,CommentForm
+from django.contrib.auth.decorators import login_required 
+from django.contrib.auth import authenticate,login,logout 
+from django.contrib.auth.forms import UserCreationForm 
 
 # Create your views here.
 def index(request):
@@ -50,10 +54,61 @@ def update_profile(request):
 
 
 
+
+
 def sign_up(request):
-    return render(request, 'auth/sign_up.html')
+    form = UserCreationForm()
+    
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            
+            login(request, user) 
+            return redirect('index')
+        
+        else:
+            messages.error(request, 'Registration failed!!')
+
+    context = {'form': form}
+    return render(request, 'auth/sign_up.html', context)
+
+
+
 
 def login(request):
-    return render(request, 'auth/login.html')
+    # page = 'login'
+    if request.user.is_authenticated:
+        return redirect('index')
+
+    if request.method == 'POST':
+        username = request.POST.get('username').lower()
+        password = request.POST.get('password')
+        
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'User do not exist')
+            
+        user = authenticate(request, username=username, password=password) 
+
+
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+
+        else:
+            messages.error(request, 'Wrong username or password')
+
+    context = {}
+    return render(request, 'auth/login.html', context)
+
+
+
+def logout(request):
+    logout(request)
+    return redirect('index')
 
 
